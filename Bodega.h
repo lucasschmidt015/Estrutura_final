81cced07cf24e989b066251369270e02f68d1dd4
GeovaneMachado <geovanemachado9@gmail.com>
2021-10-04 19:38:15 -0300

    nome do tipo de função

--- Bodega.h
+++ Bodega.h
@@ -14,7 +14,7 @@ struct HEAD{
     bebida *first;
     bebida *last;
 };
-typedef struct HEAD head;   
+typedef struct HEAD body;   
 int menu(){
     int menu1;
     printf("[1]-Cadastrar bebida\n[2]-Listar bebidas\n[3]-Buscar bebidas\n[4]-Excluir bebidas\n[5]-Comprar Bebidas\n[6]-Vender Bebidas\n[7]-Sair do Sistema\n");
@@ -27,6 +27,6 @@ int menu(){
     return menu1;
 }
 
-head cad_bebidas(head *first){
-    head *aux;
+body cad_bebidas(body *first){
+    body *aux;
 }
