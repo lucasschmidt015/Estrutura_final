ec254a72f6377a5eac936b6568215319e3ac7e6d
Lucas Schmidt <lucas.schmidt015@gmail.com>
2021-10-04 19:36:33 -0300

    fsfa

--- Bodega.h
+++ Bodega.h
@@ -15,17 +15,18 @@ struct HEAD{
     bebida *last;
 };
 typedef struct HEAD head;   
+int menu(){
+    int menu1;
+    printf("[1]-Cadastrar bebida\n[2]-Listar bebidas\n[3]-Buscar bebidas\n[4]-Excluir bebidas\n[5]-Comprar Bebidas\n[6]-Vender Bebidas\n[7]-Sair do Sistema\n");
+    scanf("%d", &menu1);
+    while(menu1<1||menu1>7){
+        printf("Opcao invalida\n");
+        printf("[1]-Cadastrar bebida\n[2]-Listar bebidas\n[3]-Buscar bebidas\n[4]-Excluir bebidas\n[5]-Comprar Bebidas\n[6]-Vender Bebidas\n[7]-Sair do Sistema\n");
+        scanf("%d", &menu1);
+    }
+    return menu1;
+}
 
 head cad_bebidas(head *first){
     head *aux;
-}
-
-int main(){
-    head pointers;
-    int menu1;
-    pointers.first=NULL;
-    pointers.last=NULL;
-    printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-BEM VINDO A ESSA BODEGA-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
-    menu1 = menu();
-    return 0;
 }
--- bar.c
+++ bar.c
@@ -3,14 +3,13 @@
 #include <string.h>
 #include "Bodega.h"
 
-int menu(){
+
+int main(){
+    head pointers;
     int menu1;
-    printf("[1]-Cadastrar bebida\n[2]-Listar bebidas\n[3]-Buscar bebidas\n[4]-Excluir bebidas\n[5]-Comprar Bebidas\n[6]-Vender Bebidas\n[7]-Sair do Sistema\n");
-    scanf("%d", &menu1);
-    while(menu1<1||menu1>7){
-        printf("Opcao invalida\n");
-        printf("[1]-Cadastrar bebida\n[2]-Listar bebidas\n[3]-Buscar bebidas\n[4]-Excluir bebidas\n[5]-Comprar Bebidas\n[6]-Vender Bebidas\n[7]-Sair do Sistema\n");
-        scanf("%d", &menu1);
-    }
-    return menu1;
+    pointers.first=NULL;
+    pointers.last=NULL;
+    printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-BEM VINDO A ESSA BODEGA-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
+    menu1 = menu();
+    return 0;
 }
