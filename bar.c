ddb160de6dc399d61b9c295ae700b0f75c6bccde
GeovaneMachado <geovanemachado9@gmail.com>
2021-10-05 20:47:27 -0300

    Arrumando interface

--- bar.c
+++ bar.c
@@ -15,33 +15,37 @@ int main(){
     printf("=================================================================================\n");
     while (menu1!=7)
     {
+        
         menu1 = menu(); 
         switch (menu1)
         {
-        case 1:
-            system("cls");
-            beritas = cadastro_bebida(beritas);
-            break;
-        case 2:
-            system("cls");
-            print_bebida(beritas);
-            break;    
-        case 3:
-            system("cls");
-            buscar_bebida(beritas);
-            break;
-        case 4:
-            system("cls");
-            beritas = excluir(beritas);
-            break;
-        case 5:
-            comprar_bebida(beritas);
-            break;    
-        case 6:
-            break;
-        default:
-            break;
+            case 1:
+                system("cls");
+                beritas = cadastro_bebida(beritas);
+                getch();
+                break;
+            case 2:
+                system("cls");
+                print_bebida(beritas);
+                getch();
+                break;    
+            case 3:
+                system("cls");
+                buscar_bebida(beritas);
+                break;
+            case 4:
+                system("cls");
+                beritas = excluir(beritas);
+                break;
+            case 5:
+                comprar_bebida(beritas);
+                break;    
+            case 6:
+                break;
+            default:
+                break;
         }
+        system("cls");
     }
     
     return 0;
